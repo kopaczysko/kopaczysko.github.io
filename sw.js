4dcbaf22123e1692a36093e59f55e41a983807da
kopaczysko <61144431+kopaczysko@users.noreply.github.com>
2020-02-19 12:29:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,18 +6,18 @@ self.addEventListener('activate', (event) => {
   console.log('[Service Worker] Activate');
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).then((resp) =>
-      {
-        if(!resp.ok)
-        {
-          return new Response('<p>Hello from your friendly neighbourhood service worker!</p>', {
-              headers: { 'Content-Type': 'text/html' }
-            });
-        } 
-      return resp;
-      }  
-      )
-	);
-});
+// self.addEventListener('fetch', (event) => {
+  // event.respondWith(
+    // fetch(event.request).then((resp) =>
+      // {
+        // if(!resp.ok)
+        // {
+          // return new Response('<p>Hello from your friendly neighbourhood service worker!</p>', {
+              // headers: { 'Content-Type': 'text/html' }
+            // });
+        // } 
+      // return resp;
+      // }  
+      // )
+	// );
+// });
