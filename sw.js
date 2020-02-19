ee7ba5b2e951130f0b5db6f06ef9d15dc64110e0
kopaczysko <61144431+kopaczysko@users.noreply.github.com>
2020-02-19 12:55:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,35 +7,20 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-	console.log('[Service Worker] Fetch v2');
-	event.respondWith(fetch(event.request)
-	  .then( (resp) => { 
-		if(!resp.ok)
-		{
-			return new Response('<p>Brak internetów ;( Ale ServiceWorker sobie działa.</p>', {
-                          headers: { 'Content-Type': 'text/html' }
-                        });
-		}
-		return resp; 
-	   })
-	 .catch(() =>  { return new Response('<p>Brak internetów ;( Ale ServiceWorker sobie działa.</p>', {
-                          headers: { 'Content-Type': 'text/html' }
-		 }); 
-		}
-		)
-		);
-	
-  // event.respondWith(
-    // fetch(event.request).then((resp) =>
-      // {
-        // if(!resp.ok)
-        // {
-          // return new Response('<p>Hello from your friendly neighbourhood service worker!</p>', {
-              // headers: { 'Content-Type': 'text/html' }
-            // });
-        // } 
-      // return resp;
-      // }  
-      // )
-	// );
+  console.log('[Service Worker] Fetch v2');
+  event.respondWith(fetch(event.request)
+    .then((resp) => {
+      if (!resp.ok) {
+        return new Response('<p>Brak internetów ;( Ale ServiceWorker sobie działa.</p>', {
+          headers: { 'Content-Type': 'text/html' }
+        });
+      }
+      return resp;
+    })
+    .catch(() => {
+      return new Response('<p>Brak internetów ;( Ale ServiceWorker sobie działa.</p>', {
+        headers: { 'Content-Type': 'text/html' }
+      });
+    })
+  );
 });
