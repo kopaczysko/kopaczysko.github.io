5cc44f8662fc9d39e2e71c892f38ca8d493538b3
kopaczysko <61144431+kopaczysko@users.noreply.github.com>
2020-02-19 12:20:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,10 +8,16 @@ self.addEventListener('activate', function(event)) {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request) ||
+    fetch(event.request).then((resp) =>
+    {
+      if(!resp.ok)
+      {
+        return 
         new Response('<p>Hello from your friendly neighbourhood service worker!</p>', {
           headers: { 'Content-Type': 'text/html' }
         });
-      
+      }
+      return resp;
+    }
       ));
 });
